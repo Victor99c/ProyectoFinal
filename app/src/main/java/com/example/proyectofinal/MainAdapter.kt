51b54865723579a52d0b6f8b04d7d9b17d722a08
Victor99c <cova981015@gmail.com>
2022-06-05 11:42:06 -0500

    Proyecto

--- app/src/main/java/com/example/proyectofinal/MainAdapter.kt
+++ app/src/main/java/com/example/proyectofinal/MainAdapter.kt
@@ -4,15 +4,17 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.json.JSONObject
-
+import com.example.proyectofinal.ProductoCallback
 import com.example.proyectofinal.databinding.ItemProductoBinding
+import com.example.proyectofinal.remote.ProductoEntry
 import com.squareup.picasso.Picasso
+import retrofit2.Callback
 
 
-class MainAdapter(private val li: Array<JSONObject>): RecyclerView.Adapter<MainAdapter.MainHolder>() {
+class MainAdapter(private val li: Array<JSONObject>, private val callBack: ProductoCallback): RecyclerView.Adapter<MainAdapter.MainHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainAdapter.MainHolder {
         val binding = ItemProductoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MainHolder(binding)
+        return MainHolder(binding,callBack)
     }
 
     override fun onBindViewHolder(holder: MainAdapter.MainHolder, position: Int){
@@ -22,7 +24,7 @@ class MainAdapter(private val li: Array<JSONObject>): RecyclerView.Adapter<MainA
     }
 
     override fun getItemCount(): Int = li.size
-    class MainHolder(val binding: ItemProductoBinding): RecyclerView.ViewHolder(binding.root){
+    class MainHolder(val binding: ItemProductoBinding, val callBack: ProductoCallback): RecyclerView.ViewHolder(binding.root){
         fun render(resp: JSONObject){
             binding.NoProduct.setText(resp.getString("title"))
             binding.Descripcio.setText(resp.getString("description"))
@@ -32,6 +34,9 @@ class MainAdapter(private val li: Array<JSONObject>): RecyclerView.Adapter<MainA
             if (link != null && !link.isEmpty()) {
                 Picasso.get().load(link).into(imagen)
             }
+              binding.detalle.setOnClickListener {
+             callBack.onClick(resp.getString("id"))
+          }
         }
     }
 }
--- app/src/main/java/com/example/proyectofinal/RandomFragment.kt
+++ app/src/main/java/com/example/proyectofinal/RandomFragment.kt
@@ -15,7 +15,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class RandomFragment : Fragment() {
+class RandomFragment : Fragment(), ProductoCallback {
 
 private lateinit var binding: FragmentRandomBinding
     override fun onCreateView(
@@ -42,9 +42,9 @@ private lateinit var binding: FragmentRandomBinding
                 override fun onResponse(call: Call<ProductoEntry>, response: Response<ProductoEntry>) {
                     Log.d("resp", response.body().toString())
                     val respuesta= response.body()
-                    li.set(i,JSONObject("{\"title\": \"${respuesta?.nom.toString()}\", \"price\":\"${respuesta?.precio.toString()}\",\"description\":\"${respuesta?.descripcion.toString()}\",\"image\":\"${respuesta?.imagen.toString()}\"}"),)
+                    li.set(i,JSONObject("{\"id\": \"${respuesta?.id.toString()}\",\"title\": \"${respuesta?.nom.toString()}\", \"price\":\"${respuesta?.precio.toString()}\",\"description\":\"${respuesta?.descripcion.toString()}\",\"image\":\"${respuesta?.imagen.toString()}\"}"),)
                     val d = Log.d("res", li.toString())
-                    val adapter = MainAdapter(li)
+                    val adapter = MainAdapter(li,this@RandomFragment)
                     binding.productos.adapter = adapter
                 }
                 override fun onFailure(call: Call<ProductoEntry>, t: Throwable) {
@@ -62,6 +62,10 @@ private lateinit var binding: FragmentRandomBinding
 
     }
 
+    override fun onClick(id: String) {
+        val directio = RandomFragmentDirections.actionRandomFragmentToDetallesBusquedaFragment(id)
+        findNavController().navigate(directio)
+    }
 
 
 }
